79dd60c0f17e53c00c7fb6b8f43de0acb09e573f
ROKO02 <90667780+ROKO02@users.noreply.github.com>
2022-05-13 13:30:07 +0900

    Add files via upload

---
+++ js/1.js
@@ -0,0 +1,10 @@
+var MOgnb = document.querySelector("header_cont");
+
+function openMoGnb(){
+    console.log("ok")
+    MOgnb.style.display = "block";
+}
+function closeMoGnb(){
+    console.log("ok")
+    MOgnb.style.display = "none";
+}
